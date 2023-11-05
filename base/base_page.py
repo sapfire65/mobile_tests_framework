c781423f4efddacb7e032978667ef91c834fbda0
Sapfire65 <cot65@yandex.ru>
2023-11-06 02:14:57 +0300

    update functions in base_page.py

--- base/base_page.py
+++ base/base_page.py
@@ -1,25 +1,51 @@
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from appium.webdriver import WebElement, Remote
+from appium.webdriver.common.appiumby import AppiumBy
 from time import sleep
 
 class BasePage:
-    CHROME = ('xpath', '//android.widget.TextView[@content-desc="Chrome"]')
+
+    CHROME = (AppiumBy.XPATH, '//android.widget.TextView[@content-desc="Chrome"]')
+    LOGO_GOOGLE = (AppiumBy.CLASS_NAME, 'android.widget.FrameLayout')
+    SEARSH_INPUT = (AppiumBy.ID, 'com.android.chrome:id/search_box_text')
 
 
     def __init__(self, driver:Remote):
         self.driver = driver
-        self.wait = WebDriverWait(driver, 200, 1)
+        self.wait = WebDriverWait(driver, 20, 1)
 
 
-    def open_chrome(self):
-        element = self.CHROME
-        el: WebElement = self.wait.until(EC.element_to_be_clickable(element))
+    def click_obj(self, locator):
+        """Нажать по кликабельному объекту"""
+        el: WebElement = self.wait.until(EC.element_to_be_clickable(locator))
         el.click()
-        sleep(2)
-        a = self.driver.window_handles
-        print(a)
 
+    def check_element_located(self, locator: str):
+        """
+        Проверка видимости элемента
+
+        Params:
+            locator (str)
+        """
+        self.wait.until(EC.visibility_of_element_located(locator))
+
+
+    def send(self, locator: str, text: str):
+        """
+        Ввод текста
+
+        Params:
+            locator (str)
+            text (str) - yuor masage
+        """
+        el: WebElement = self.wait.until(EC.element_to_be_clickable(locator))
+        el.send_keys(text)
+
+    def press_enter(self):
+        # self.driver.is_keyboard_shown()
+        self.driver.press_keycode(66)
+        sleep(2)
 
 
     def swipe_too(self, target_direction: str = 'MOVE_UP', repeat: int = 1):
@@ -40,6 +66,13 @@ class BasePage:
                 self.driver.swipe(1200, 1000, 200, 1000, duration=1000) # swaip_down
             elif target_direction == 'MOVE_RIGHT':
                 self.driver.swipe(200, 1000, 1200, 1000, duration=1000) # swaip_down
+
+
+    def open_chrome(self):
+        self.click_obj(self.CHROME)
+        self.check_element_located(self.LOGO_GOOGLE)
+        self.send(locator=self.SEARSH_INPUT, text='Hoo Hoo Hoo')
+        self.press_enter()
 
 
 
